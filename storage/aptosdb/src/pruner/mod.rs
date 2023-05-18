2deb693ddd9334d6eb92cefa5ee08c42549f2ee1
rig410 <dmitryyakushev@dfinance.co>
2023-05-18 10:56:55 +0300

    Makes node components public.

--- storage/aptosdb/src/pruner/mod.rs
+++ storage/aptosdb/src/pruner/mod.rs
@@ -2,21 +2,21 @@
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-pub(crate) mod db_pruner;
-pub(crate) mod db_sub_pruner;
-pub(crate) mod event_store;
-pub(crate) mod ledger_pruner_worker;
-pub(crate) mod ledger_store;
-pub(crate) mod pruner_manager;
+pub mod db_pruner;
+pub mod db_sub_pruner;
+pub mod event_store;
+pub mod ledger_pruner_worker;
+pub mod ledger_store;
+pub mod pruner_manager;
 pub mod pruner_utils;
-pub(crate) mod state_kv_pruner;
-pub(crate) mod state_kv_pruner_worker;
-pub(crate) mod state_merkle_pruner_worker;
-pub(crate) mod state_store;
-pub(crate) mod transaction_store;
+pub mod state_kv_pruner;
+pub mod state_kv_pruner_worker;
+pub mod state_merkle_pruner_worker;
+pub mod state_store;
+pub mod transaction_store;
 
 // This module provides `Pruner` which manages a thread pruning old data in the background and is
 // meant to be triggered by other threads as they commit new data to the DB.
-pub(crate) mod ledger_pruner_manager;
-pub(crate) mod state_kv_pruner_manager;
-pub(crate) mod state_merkle_pruner_manager;
+pub mod ledger_pruner_manager;
+pub mod state_kv_pruner_manager;
+pub mod state_merkle_pruner_manager;
